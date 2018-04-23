bac2cbc024867f8c877be694656e223a33044bf8
erikvanmechelen <erik.vanmechelen@gmail.com>
2018-04-22 21:25:26 -0500

    Update background.js

--- callextension/background.js
+++ callextension/background.js
@@ -7,4 +7,9 @@
       "persistent": false
     },
     "manifest_version": 2
+    chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log("The color is green.");
+    });
+  });
   }
