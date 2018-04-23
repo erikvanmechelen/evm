d4d37330091c2ed474e86f9d41e4ef01f77802ff
erikvanmechelen <erik.vanmechelen@gmail.com>
2018-04-22 21:32:16 -0500

    Update background.js

--- callextension/background.js
+++ callextension/background.js
@@ -1,7 +1,13 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
+  {
+    "name": "Getting Started Example",
+    "version": "1.0",
+    "description": "Build an Extension!",
+    "background": {
+      "scripts": ["background.js"],
+      "persistent": false
+    },
+    "manifest_version": 2
+  }
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
